c10029ed5bb76e23776de54e869c65a63a30e60f
Deniz Celebi <5246652+derDeno@users.noreply.github.com>
2023-03-27 16:06:10 +0200

    bug fix in response.dto

--- src/articles/dto/response.dto.ts
+++ src/articles/dto/response.dto.ts
@@ -1,16 +1,13 @@
-import { Exclude } from 'class-transformer';
 import { TagsEntity } from 'src/entities/tags.entity';
 
 export class ResponseDto {
   public id: number;
   public title: string;
+  public content: string;
   public author: string;
   public authorAge: number;
   public date_created: number;
   public date_publish: number;
   public date_expire: number;
   public tags: TagsEntity[];
-
-  @Exclude()
-  public content: string;
 }
